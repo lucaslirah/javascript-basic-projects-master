91c0a922edd15da477bf2b0f312aa4a8dfd1c792
lucaslira <lucasliraone@gmail.com>
2024-08-28 20:11:55 -0300

    nav-bar project solved

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,13 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+// select elements
+const navToggleBtn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+// add event listener to toggle nav links on click
+navToggleBtn.addEventListener("click", function () {
+    links.classList.toggle("show-links");
+});
